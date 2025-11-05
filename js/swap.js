9d506a134482c791753152e773354b9a73053630
leonardoavila <leonardofabricioavila@gmail.com>
2025-11-05 11:18:52 -0400

    responsividade para tablets

--- js/swap.js
+++ js/swap.js
@@ -1,8 +1,8 @@
 
 export default function swapScreen() {
-    const isDesktop = window.matchMedia('(min-width: 1200px)').matches;
+    const isDesktopOrTablet = window.matchMedia('(min-width: 768px)').matches;
 
-    if (isDesktop) {
+    if (isDesktopOrTablet) {
         function Swap() {
             const outputLibras = document.querySelector('.output-libras-section');
             outputLibras.classList.toggle('swap');
