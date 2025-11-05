94c5a2e401e05fe0cd2adf0fceac5fbe7a6c02eb
leonardoavila <leonardofabricioavila@gmail.com>
2025-11-05 09:19:23 -0400

    codigo de tela flutuante desativado.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-
-
 import speechRecognition from "./SpeechRecognition.js";
-import floatScreen from "./floatScreen.js";
 import bodyAnimation from "./bodyAnimations.js";
 
 speechRecognition();
 bodyAnimation();
-floatScreen();
